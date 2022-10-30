6cfc53c2609021b4988d59f0f7dda7e98b1f63e2
bogoslavskiy <luice96@yahoo.com>
2022-10-31 02:15:37 +0400

    fix: createMediaStyleVars export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createSteezy } from "./createSteezy";
 export { createDynamicStyleVars } from './createDynamicStyleVars';
+export { createMediaStyleVars } from './createMediaStyleVars';
 export { StyleProp, WithStyleProp } from './types';
